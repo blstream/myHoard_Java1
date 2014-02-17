f069caf26ecbbcf3c9e19654a25d2b09d1e4a22c
Krzysztof Piasecki <krzyszek@gmail.com>
2014-02-17 14:04:43 +0100

    #93 : added 100_Collections.sql file to Config directory

---
+++ Config/SQL/Sprint1/100_Collections.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE IF NOT EXISTS `Collections` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8_bin NOT NULL,
+  `decription` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  `tags` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  `items_number` int(11) NOT NULL DEFAULT '0',
+  `created_date` datetime NOT NULL,
+  `modified_date` datetime NOT NULL,
+  `owner` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
