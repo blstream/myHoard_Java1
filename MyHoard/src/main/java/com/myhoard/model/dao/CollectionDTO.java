4d76bb194efaeb40477dacbfac7ebebeac216832
Mateusz Torba <mateusztorba@gmail.com>
2014-02-20 18:52:46 +0100

    wstepna wersja dto

---
+++ MyHoard/src/main/java/com/myhoard/model/dao/CollectionDTO.java
@@ -0,0 +1,173 @@
+package com.myhoard.model.dao;
+
+import java.util.Date;
+
+public class CollectionDTO {
+
+	private String id;
+	private String name;
+	private String description;
+	private String tags;
+	private int itemsNumber;
+	private Date createdDate;
+	private Date modifiedDate;
+	private String owner;
+
+	public CollectionDTO() {
+		super();
+	}
+
+	public CollectionDTO(String id, String name, String description,
+			String tags, int itemsNumber, Date createdDate, Date modifiedDate,
+			String owner) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.tags = tags;
+		this.itemsNumber = itemsNumber;
+		this.createdDate = createdDate;
+		this.modifiedDate = modifiedDate;
+		this.owner = owner;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getTags() {
+		return tags;
+	}
+
+	public void setTags(String tags) {
+		this.tags = tags;
+	}
+
+	public int getItemsNumber() {
+		return itemsNumber;
+	}
+
+	public void setItemsNumber(int itemsNumber) {
+		this.itemsNumber = itemsNumber;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Date getModifiedDate() {
+		return modifiedDate;
+	}
+
+	public void setModifiedDate(Date modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((createdDate == null) ? 0 : createdDate.hashCode());
+		result = prime * result
+				+ ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + itemsNumber;
+		result = prime * result
+				+ ((modifiedDate == null) ? 0 : modifiedDate.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((owner == null) ? 0 : owner.hashCode());
+		result = prime * result + ((tags == null) ? 0 : tags.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CollectionDTO other = (CollectionDTO) obj;
+		if (createdDate == null) {
+			if (other.createdDate != null)
+				return false;
+		} else if (!createdDate.equals(other.createdDate))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (itemsNumber != other.itemsNumber)
+			return false;
+		if (modifiedDate == null) {
+			if (other.modifiedDate != null)
+				return false;
+		} else if (!modifiedDate.equals(other.modifiedDate))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (owner == null) {
+			if (other.owner != null)
+				return false;
+		} else if (!owner.equals(other.owner))
+			return false;
+		if (tags == null) {
+			if (other.tags != null)
+				return false;
+		} else if (!tags.equals(other.tags))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "CollectionDTO [id=" + id + ", name=" + name + ", description="
+				+ description + ", tags=" + tags + ", itemsNumber="
+				+ itemsNumber + ", createdDate=" + createdDate
+				+ ", modifiedDate=" + modifiedDate + ", owner=" + owner + "]";
+	}
+
+}
