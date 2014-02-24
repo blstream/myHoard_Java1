6899688e8b2013e578718c0688362232a3c33166
Mateusz Torba <mateusztorba@gmail.com>
2014-02-24 20:31:34 +0100

    exception

--- src/main/java/com/blstream/myhoard/controller/CollectionErrorHandler.java
+++ src/main/java/com/blstream/myhoard/controller/CollectionErrorHandler.java
@@ -1,8 +1,11 @@
 package com.blstream.myhoard.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -10,6 +13,7 @@ import com.blstream.myhoard.exception.CollectionException;
 import com.blstream.myhoard.exception.ErrorCode;
 
 @ControllerAdvice
+@RequestMapping("/collections")
 public class CollectionErrorHandler {
 
 	@ExceptionHandler(CollectionException.class)
@@ -26,6 +30,26 @@ public class CollectionErrorHandler {
 	public ErrorCode handleCollectionExceptionA(
 			org.hibernate.PropertyValueException ex) {
 		return new ErrorCode(111);
+	}
+
+	@ExceptionHandler
+	@ResponseBody
+	@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+	public ErrorCode handleException(Exception exception,
+			HttpServletRequest request) {
+
+		String method = request.getMethod();
+
+		switch (method) {
+		case "POST":
+			return new ErrorCode(400);
+		case "PUT":
+			return new ErrorCode(111);
+		default:
+			return new ErrorCode(0);
+
+		}
+
 	}
 
 }
