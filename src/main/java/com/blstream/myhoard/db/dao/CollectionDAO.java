574708e231256eadfc136cfaa9fcd6374688f0e7
Mateusz Torba <mateusztorba@gmail.com>
2014-03-10 10:27:36 +0100

    zapisywanie tagow przy create

--- src/main/java/com/blstream/myhoard/db/dao/CollectionDAO.java
+++ src/main/java/com/blstream/myhoard/db/dao/CollectionDAO.java
@@ -1,7 +1,10 @@
 package com.blstream.myhoard.db.dao;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
