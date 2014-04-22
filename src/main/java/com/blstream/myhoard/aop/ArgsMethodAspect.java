c3d92aa6243c5b90dbe1942ddc6570c60de147ac
Mateusz Torba <mateusztorba@gmail.com>
2014-04-22 15:05:58 +0200

    AOP - wartosci argumentow

---
+++ src/main/java/com/blstream/myhoard/aop/ArgsMethodAspect.java
@@ -0,0 +1,41 @@
+package com.blstream.myhoard.aop;
+
+import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+
+@Aspect
+public class ArgsMethodAspect {
+
+	private Logger logger = Logger.getLogger(ArgsMethodAspect.class
+			.getCanonicalName());
+
+	@Pointcut("within(com.blstream.myhoard..*)")
+	public void getArgs() {
+
+	}
+
+	@Before("getArgs()")
+	public void getArgsDemo(JoinPoint joinPoint) {
+		logger.debug("Class name: "
+				+ joinPoint.getTarget().getClass().getName());
+		logger.debug("Method name: " + joinPoint.getSignature().getName());
+		MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+		String[] parameterNames = signature.getParameterNames();
+		if (parameterNames != null) {
+			logger.debug("Method args name: ");
+			for (String string : parameterNames) {
+				logger.debug(string);
+			}
+		}
+		logger.debug("Method args value: ");
+		for (Object arg : joinPoint.getArgs()) {
+			logger.debug(arg);
+		}
+		logger.debug("\n");
+	}
+
+}
