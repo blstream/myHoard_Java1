3b8cf0aa3ffa6ce0e38492daf87271c8ca92859a
rtatol <rtatol@gmail.com>
2014-04-29 21:35:38 +0200

    bugfix #668

--- src/main/java/com/blstream/myhoard/biz/validator/ItemValidator.java
+++ src/main/java/com/blstream/myhoard/biz/validator/ItemValidator.java
@@ -5,20 +5,22 @@ import com.blstream.myhoard.biz.model.ItemDTO;
 import com.blstream.myhoard.biz.service.ItemService;
 import com.blstream.myhoard.exception.MyHoardException;
 import com.blstream.myhoard.exception.ValidatorException;
-
 import java.util.HashMap;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Component;
 
 @Component
 public class ItemValidator extends AbstractValidator {
 
-    private final String KEY_NAME = "name";
-    private final String KEY_PATTERN = "pattern";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_PATTERN = "pattern";
+    private static final String KEY_LOCATION_LAT = "location.lat";
+    private static final String KEY_LOCATION_LNG = "location.lng";
 
     private static final String NAME_LENGTH = "Length of name must be between 2 and 20 characters";
     private static final String NAME_EXIST = "Item name: %s, already exist";
+    private static final String LOCATION_LAT = "lat value: %s is invalid. Latitude values must be between -90 and 90 degrees";
+    private static final String LOCATION_LNG = "lng value: %s is invalid. Longitude values must be between -180 and 180 degrees";
     
     @Autowired
     private ItemService itemService;
@@ -49,6 +51,15 @@ public class ItemValidator extends AbstractValidator {
             errorMessages.put(KEY_NAME, String.format(MESSAGE_LENGTH_MIN_MAX, 2, 50));
         } else if (!itemService.isUniqueNameOfCollectionItem(itemDTO.getName(), Integer.parseInt(itemDTO.getCollection()))) {
             errorMessages.put(KEY_NAME, String.format(NAME_EXIST, itemDTO.getName()));
+        }
+
+        if (itemDTO.getLocation() != null) {
+            if (itemDTO.getLocation().getLat() == null || itemDTO.getLocation().getLat() < -90 || itemDTO.getLocation().getLat() > 90) {
+                errorMessages.put(KEY_LOCATION_LAT, String.format(LOCATION_LAT, itemDTO.getLocation().getLat()));
+            }
+            if (itemDTO.getLocation().getLng() == null || itemDTO.getLocation().getLng() < -180 || itemDTO.getLocation().getLng() > 180) {
+                errorMessages.put(KEY_LOCATION_LNG, String.format(LOCATION_LNG, itemDTO.getLocation().getLng()));
+            }
         }
     }
 
